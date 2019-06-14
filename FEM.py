0c8cadc01c1b10fda24a5bc5d0b4b49530708c8e
Wilbert den Hertog <wmdenhertog@gmail.com>
2019-06-14 22:06:04 +0200

    Code revision
    Removed some depricated functions and added some comments with explanations

--- FEM.py
+++ FEM.py
@@ -1,11 +1,13 @@
 from main_programv2 import *
-import xlsxwriter
 
 
 def calculate_basisfunctions(e):
-    bf1 = np.zeros(shape=3)  # linear basis functions in the form: phi_i(x)=a_i_0+a_i_x*x+a_i_y*y
-    bf2 = np.zeros(shape=3)  # for every point in the element there is 1 basis function. The basisfunction array has
-    bf3 = np.zeros(shape=3)  # the following shape: (a_i_0,a_i_x,a_i_y).
+    # Linear basis functions in the form: phi_i(x)=a_i_0+a_i_x*x+a_i_y*y.
+    # For every point in the element there is 1 basis function.
+    # The basisfunction array has the following shape: (a_i_0,a_i_x,a_i_y).
+    bf1 = np.zeros(shape=3)
+    bf2 = np.zeros(shape=3)
+    bf3 = np.zeros(shape=3)
     bf1[1] = 1 / (2 * e.size) * (e.points[1].coordinates[1] - e.points[2].coordinates[1])
     bf1[2] = 1 / (2 * e.size) * (e.points[2].coordinates[0] - e.points[1].coordinates[0])
     bf1[0] = 1 - bf1[1] * e.points[0].coordinates[0] - bf1[2] * e.points[0].coordinates[1]
@@ -38,7 +40,6 @@ def calculate_element_matrices(e, lbda, mu):
 
 def stiffness_matrix(grid, lbda, mu):
     # calculation of the big stifness matrix
-    # f=(fx_top, fx_left, fx_bottom, fx_right, fy_top, fy_left, fy_bottom, fy_right)
     sxx = np.zeros(shape=(len(grid.points), len(grid.points)))
     sxy = np.zeros(shape=(len(grid.points), len(grid.points)))
     syx = np.zeros(shape=(len(grid.points), len(grid.points)))
@@ -52,34 +53,12 @@ def stiffness_matrix(grid, lbda, mu):
                 syx[e.points[i].index][e.points[j].index] += e_syx[i][j]
                 syy[e.points[i].index][e.points[j].index] += e_syy[i][j]
     s = np.hstack((np.vstack((sxx, syx)), np.vstack((sxy, syy))))
-
-    # TODO: boundary conditions
-    # q = np.zeros(shape=(len(grid.points),1))
     return s
 
 
-def toexcel(s):
-    # just a function to check the layout of the matrix quickly
-    workbook = xlsxwriter.Workbook('stiffnessmatrix.xlsx')
-    worksheet = workbook.add_worksheet()
-    for i in range(len(s)):
-        for j in range(len(s)):
-            worksheet.write(i, j, s[i][j])
-    workbook.close()
-    return
-
-
-def checksymmetric(s):
-    # check if the (non-)zero places are symmetric
-    # (N.B. this does not necessarily mean that the matrix itself is symmetric!)
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if (s[i][j] == 0 and s[j][i] != 0) or (s[i][j] != 0 and s[j][i] == 0):
-                return False
-    return True
-
-
 def meshrelaxation(grid, u, lbda, mu):
+    # This function calculates the solution of the mesh relaxation problem, given the grid, the imposed deformations u,
+    # lambda and mu.
     s = stiffness_matrix(grid, lbda, mu)
 
     # restrict the vertical movement of the bottom and top boundaries and the horizontal movement of the left
@@ -105,6 +84,8 @@ def meshrelaxation(grid, u, lbda, mu):
             s[grid.zpointongrid[i].index + len(grid.points)][j] = 0
         s[grid.zpointongrid[i].index][grid.zpointongrid[i].index] = 1
         s[grid.zpointongrid[i].index + len(grid.points)][grid.zpointongrid[i].index + len(grid.points)] = 1
+
+    # deformations into the stiffnessmatrix
     for i in range(len(u)):
         if u[i] != 0:
             for j in range(2 * len(grid.points)):
@@ -155,7 +136,7 @@ def qualitycomparisonplot(femstats, efrstats, lbda, mu, method, gridsize, thresh
     return
 
 
-def qualitymeasure(old_mesh, new_mesh):
+def qualitymeasure(old_mesh, new_mesh, lbda):
     # calculates the quality of the new mesh compared to the old mesh:
     # \sum_{elements} (((1+(oldarea-newarea)/oldarea)^2)*(1+(oldskew-newskew)^2))
     quality = 0
@@ -169,6 +150,8 @@ def qualitymeasure(old_mesh, new_mesh):
 
 
 def calc_spfr_efr(gridsize, farr):
+    # Calculate the startgrid for FEM (shortest path fit with redistribution),
+    # and the reference grid of the spring model (Shortest path fit with Euler forward relaxation).
     newtriang = False
     flip = False
     simplepointrelax = False
@@ -192,12 +175,15 @@ def calc_spfr_efr(gridsize, farr):
     return spfr, efr
 
 
-def fem_meshfit(gridsize, threshold, lbda, mu, farr, spfr, efr):
+def fem_meshfit(gridsize, threshold, lbda, mu, farr, spfr):
+    # The actual meshfitting algorithm. Farr is the function used, and spfr is the Shortest Path Fit with Redistribution
+    # mesh, the startsituation for FEM.
     m = mesh(gridsize, threshold)
     standardmesh = mesh(gridsize, threshold)
     m.plot()
     m.initzeroinfo(farr[0])
 
+    # Calculate the differences between the standard grid and spfr to find the deformations.
     u_x_fit = np.zeros(len(m.points))
     u_y_fit = np.zeros(len(m.points))
     for i in range(len(m.points)):
@@ -224,15 +210,14 @@ def fem_meshfit(gridsize, threshold, lbda, mu, farr, spfr, efr):
         el.skewness = el.calcskew()
 
     m.plotzeroz('figures_FEM/FEMfit.png', mode=2)
-    # femstats, efrstats = qualitycomparison(m, efr)
-    # qualitycomparisonplot(femstats, efrstats, lbda, mu, farr[0].__name__)
-    quality = qualitymeasure(standardmesh, m)
+    quality = qualitymeasure(standardmesh, m, lbda)
     print('Quality measure = ' + str(quality))
     return m, quality
 
 
 def recursiveoptimisation(lower_boundary, upper_boundary, gridsize, threshold, farr, spfr, efr, resultlist, tol=1e-5,
                           h=None, c=None, d=None, fc=None, fd=None):
+    # The algorithm to optimise lambda recursively
     invphi = (math.sqrt(5) - 1) / 2  # 1/phi
     invphi2 = (3 - math.sqrt(5)) / 2  # 1/phi^2
     (lower_boundary, upper_boundary) = (min(lower_boundary, upper_boundary), max(lower_boundary, upper_boundary))
@@ -245,9 +230,9 @@ def recursiveoptimisation(lower_boundary, upper_boundary, gridsize, threshold, f
     if d is None:
         d = lower_boundary + invphi * h
     if fc is None:
-        mc, fc = fem_meshfit(gridsize, threshold, c, 1, farr, spfr, efr)
+        mc, fc = fem_meshfit(gridsize, threshold, c, 1, farr, spfr)
     if fd is None:
-        md, fd = fem_meshfit(gridsize, threshold, d, 1, farr, spfr, efr)
+        md, fd = fem_meshfit(gridsize, threshold, d, 1, farr, spfr)
     if fc < fd:
         resultlist.append([c, fc])
         print('lower boundary: ' + str(lower_boundary) + ' upper boundary: ' + str(d))
